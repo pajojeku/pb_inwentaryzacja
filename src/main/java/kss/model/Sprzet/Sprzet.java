c4b486487ebc79a95c03e5400ebbe3100f9adb14
pajojeku <pajojeq@gmail.com>
2024-05-20 18:38:19 +0200

    test

---
+++ src/main/java/kss/model/Sprzet/Sprzet.java
@@ -0,0 +1,11 @@
+package kss.model.Sprzet;
+
+import kss.model.Wyposazenie;
+
+public class Sprzet extends Wyposazenie{
+
+    public Sprzet(String nazwa, Typ typ, Stan stan) {
+        super(nazwa, typ, stan);
+    }
+    
+}
